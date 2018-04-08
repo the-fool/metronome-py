fe433078bc30ce2120b4677214146d671f851789
thomas <sketchbang@gmail.com>
2018-04-07 22:19:11 -0500

    fix setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='python-package-example',
+    name='metronome-py',
     version='1',
     packages=find_packages(),
     license='MIT',
